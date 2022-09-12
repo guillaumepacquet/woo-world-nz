12bcdab7a0e5c629262dfa48341d3542fe6dc049
Guillaume Pacquet <guillaume.pacquet@gmail.com>
2022-09-12 19:32:00 +1200

    Add so beauty

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar app color="white" flat>
+    <v-app-bar app color="indigo darken-2" dark>
       <v-container class="py-0 fill-height">
+        <v-avatar class="mr-10" size="32">
+          <v-icon dark> mdi-kitesurfing </v-icon></v-avatar
+        >
         New Zealand Team Leaderboard
       </v-container>
     </v-app-bar>
@@ -11,6 +14,13 @@
         <router-view />
       </v-container>
     </v-main>
+    <v-footer padless color="indigo darken-2" dark>
+      <v-col class="text-center" cols="12">
+        <v-btn href="https://github.com/guillaumepacquet/woo-world-nz" text>
+          <v-icon class="mr-2">mdi-github</v-icon> source code
+        </v-btn>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 

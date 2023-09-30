eb4faead5abd30bdbea57300f4d6094b2dd991ad
Guillaume Pacquet <guillaume.pacquet@gmail.com>
2023-09-30 17:35:55 +1300

    fix edge function env variable name

--- supabase/functions/update-team/index.ts
+++ supabase/functions/update-team/index.ts
@@ -97,7 +97,7 @@ serve(async (req) => {
     try {
         const supabaseClient = createClient(
             Deno.env.get("SUPABASE_URL") ?? "",
-            Deno.env.get("SUPABASE_KEY") ?? "",
+            Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
             {
                 auth: {
                     persistSession: false //or true
